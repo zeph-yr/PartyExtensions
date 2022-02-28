af5a7d57e7093ae5a6ca62f7b20c978694c3e6a0
Zephyr <67527746+zeph-yr@users.noreply.github.com>
2022-02-28 19:09:11 +0800

    Added AppData backup

--- CustomScoreData.cs
+++ CustomScoreData.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
 
@@ -18,15 +19,34 @@ namespace PartyExtensions
         internal static Dictionary<string, CustomLeaderboard> all_scores;
         internal static Dictionary<string, CustomLeaderboard> daily_scores;
 
-        internal static string file_path = @"C:\Program Files (x86)\Steam\steamapps\common\Beat Saber\UserData\PartyExtensions\LocalScores.json";
-        internal static string daily_file_path = @"C:\Program Files (x86)\Steam\steamapps\common\Beat Saber\UserData\PartyExtensions\DailyLocalScores.json";
+        //internal static string file_path = @"C:\Program Files (x86)\Steam\steamapps\common\Beat Saber\UserData\PartyExtensions\LocalScores.json";
+        //internal static string daily_file_path = @"C:\Program Files (x86)\Steam\steamapps\common\Beat Saber\UserData\PartyExtensions\DailyLocalScores.json";
+
+        internal static string alltime_path;
+        internal static string daily_path;
 
 
         internal static void Read()
         {
             //Plugin.Log.Debug("Read");
 
-            if (!File.Exists(file_path))
+            string appdata_path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            string base_alltime_path = appdata_path + @"Low\Hyperbolic Magnetism\Beat Saber\LocalLeaderboards.dat";
+            string base_daily_path = appdata_path + @"Low\Hyperbolic Magnetism\Beat Saber\LocalDailyLeaderboards.dat";
+
+            string userdata_path = IPA.Utilities.UnityGame.UserDataPath;
+            alltime_path = userdata_path + @"\PartyExtensions\LocalScores.json";
+            daily_path = userdata_path + @"\PartyExtensions\DailyLocalScores.json";
+
+
+            //Plugin.Log.Debug("AppData: " + base_alltime_path);
+            //Plugin.Log.Debug("AppData: " + base_daily_path);
+
+            //Plugin.Log.Debug("UserData: " + alltime_path);
+            //Plugin.Log.Debug("Userata: " + daily_path);
+
+
+            if (!File.Exists(alltime_path))
             {
                 Plugin.Log.Debug("Create alltime file");
 
@@ -42,7 +62,7 @@ namespace PartyExtensions
             {
                 Plugin.Log.Debug("Alltime file exists");
 
-                string json_string = File.ReadAllText(file_path);
+                string json_string = File.ReadAllText(alltime_path);
                 //Plugin.Log.Debug(json_string);
 
                 //test_score = JsonConvert.DeserializeObject<CustomScoreData>(json_string);
@@ -56,13 +76,15 @@ namespace PartyExtensions
                 // Extra bonus: Make a backup at the start of each game but ONLY if the file is not an empty dict
                 // Without the condition, the backup would be overwritten when the user launches the game
                 // This means if the user accidentally pressed leaderboard delete and restarts the game, they will still have once chance here to grab the backup
+                // However this will not backup the base game scores *until* the second game launch after the user has set their first PartyExtension score.
                 if (json_string != "{}")
                 {
-                    File.Copy(file_path, file_path + ".bak", true);
+                    File.Copy(alltime_path, alltime_path + ".bak", true);
+                    File.Copy(base_alltime_path, userdata_path + @"\PartyExtensions\LocalLeaderboards.json.bak", true);
                 }
             }
 
-            if (!File.Exists(daily_file_path))
+            if (!File.Exists(daily_path))
             {
                 Plugin.Log.Debug("Create daily file");
 
@@ -73,13 +95,14 @@ namespace PartyExtensions
             {
                 Plugin.Log.Debug("Daily file exists");
 
-                string json_string = File.ReadAllText(daily_file_path);
+                string json_string = File.ReadAllText(daily_path);
                 daily_scores = JsonConvert.DeserializeObject<Dictionary<string, CustomLeaderboard>>(json_string);
                 //daily_scores = JsonConvert.DeserializeObject<Dictionary<string, CustomLeaderboard>>(File.ReadAllText(daily_file_path));
 
                 if (json_string != "{}")
                 {
-                    File.Copy(daily_file_path, daily_file_path + ".bak", true);
+                    File.Copy(daily_path, daily_path + ".bak", true);
+                    File.Copy(base_daily_path, userdata_path + @"\PartyExtensions\LocalDailyLeaderboards.json.bak", true);
                 }
             }
         }
@@ -95,14 +118,14 @@ namespace PartyExtensions
             //string json_string = JsonConvert.SerializeObject(all_scores);
             //Plugin.Log.Debug(json_string);
 
-            File.WriteAllText(file_path, JsonConvert.SerializeObject(all_scores));
+            File.WriteAllText(alltime_path, JsonConvert.SerializeObject(all_scores));
         }
 
         internal static void Write_Daily()
         {
             //Plugin.Log.Debug("Write Daily");
 
-            File.WriteAllText(daily_file_path, JsonConvert.SerializeObject(daily_scores));
+            File.WriteAllText(daily_path, JsonConvert.SerializeObject(daily_scores));
         }
     }
 
